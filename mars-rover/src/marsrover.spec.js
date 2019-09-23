09ce95b7e251c8803f434e7eb9c2b4dad952da06
stefan <stefan.anton@mkodo.com>
2019-09-23 21:48:11 +0100

    [marsrover] moves 2 spaces

--- mars-rover/src/marsrover.spec.js
+++ mars-rover/src/marsrover.spec.js
@@ -1,20 +1,32 @@
 describe("Mars Rover", () => {
     it("it moves", () => {
-        const rover = {
-            x: 0,
-            y: 0,
-            direction: "N",
-            execute(input) {
-                this.move();
-                return this.finalPos();
-            },
-            move() {
-                this.y += 1;
-            },
-            finalPos: function () {
-                return `${this.x}:${this.y}:${this.direction}`;
-            },
-        };
+        const rover = makeRover();
+        
         expect(rover.execute("M")).toEqual("0:1:N");
-    })
+    });
+    
+    it("it moves 2 spaces", () => {
+        const rover = makeRover();
+        
+        expect(rover.execute("MM")).toEqual("0:2:N");
+    });
 });
+
+function makeRover() {
+    return {
+        x: 0,
+        y: 0,
+        direction: "N",
+        execute(input) {
+            input.split("").forEach(() => this.move());
+            return this.finalPos();
+        },
+        move() {
+            this.y += 1;
+        },
+        finalPos: function () {
+            return `${this.x}:${this.y}:${this.direction}`;
+        },
+    };
+}
+
