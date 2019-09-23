8401e97ad7e6731d40adf08b224fc82904c55bac
stefan <stefan.anton@mkodo.com>
2019-09-23 21:48:58 +0100

    [marsrover] refactor

--- mars-rover/src/marsrover.spec.js
+++ mars-rover/src/marsrover.spec.js
@@ -1,13 +1,14 @@
 describe("Mars Rover", () => {
+    let rover;
+    beforeEach(() => {
+        rover = makeRover();
+    });
+    
     it("it moves", () => {
-        const rover = makeRover();
-        
         expect(rover.execute("M")).toEqual("0:1:N");
     });
     
     it("it moves 2 spaces", () => {
-        const rover = makeRover();
-        
         expect(rover.execute("MM")).toEqual("0:2:N");
     });
 });
