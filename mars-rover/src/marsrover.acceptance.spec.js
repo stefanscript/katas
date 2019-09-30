ff8401182f68c7060ef9790cd8d37dd1e2cad8b5
stefan <stefan.anton@mkodo.com>
2019-09-30 21:44:51 +0100

    it wraps around acceptance test

--- mars-rover/src/marsrover.acceptance.spec.js
+++ mars-rover/src/marsrover.acceptance.spec.js
@@ -9,4 +9,8 @@ describe("Mars Rover Acceptance", () => {
     it("it moves", () => {
         expect(rover.execute("MMRMMLM")).toEqual("2:3:N");
     });
+    
+    it("it wraps around", () => {
+        expect(rover.execute("MMMMMMMMMM")).toEqual("0:0:N");
+    });
 });
