cf92169cad9b463b9082f889f836b4f8fb79b3f6
stefanscript <sha32768@gmail.com>
2021-10-11 21:56:11 +0100

    [fizzbuzz] fizzbuzz 4

--- fizzbuzz/src/fizzbuzz.spec.ts
+++ fizzbuzz/src/fizzbuzz.spec.ts
@@ -1,10 +1,13 @@
 describe("FizzBuzz", () => {
-    describe("Normal string representation", () => {
+    describe("Normal cases", () => {
        it("when fizzbuzz-ing 1 it should return a string representing it", () => {
            expect(fizzbuzz(1)).toEqual("1");
        });
        it("when fizzbuzz-ing 2 it should return a string representing it", () => {
            expect(fizzbuzz(2)).toEqual("2");
+       });
+       it("when fizzbuzz-ing 4 it should return a string representing it", () => {
+           expect(fizzbuzz(4)).toEqual("4");
        });
     });
 });
@@ -13,6 +16,9 @@ describe("FizzBuzz", () => {
 function fizzbuzz(number: number) {
     if(number === 2) {
         return "2";
+    }
+    if(number === 4) {
+        return "4";
     }
     return "1";
 }
