1def12667bed5b2a4e74554b60459374aaeb28cd
stefanscript <sha32768@gmail.com>
2021-10-11 21:53:34 +0100

    [fizzbuzz] setup, fizzbuzz 1

---
+++ fizzbuzz/src/fizzbuzz.spec.ts
@@ -0,0 +1,14 @@
+
+
+describe("FizzBuzz", () => {
+    describe("Normal string representation", () => {
+       it("when fizzbuzz-ing 1 it should return a string representing it", () => {
+           expect(fizzbuzz(1)).toEqual("1");
+       });
+    });
+});
+
+
+function fizzbuzz(number: number) {
+    return "1";
+}
