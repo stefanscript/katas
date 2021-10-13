36ef15dbfbf2fbcfc63c5dbd230e9fc55bad2be1
stefanscript <sha32768@gmail.com>
2021-10-13 22:54:13 +0100

    [leapyear] common year impl

--- leapyear/src/leapyear.spec.ts
+++ leapyear/src/leapyear.spec.ts
@@ -10,8 +10,11 @@ describe("LeapYear", () => {
     it("1996 is a typical leap year", () => {
         expect(leapyear(1996)).toEqual(true);
     });
-    it("9 is a normal year", () => {
+    it("9 is a common year", () => {
         expect(leapyear(9)).toEqual(false);
+    });
+    it("2001 is a common year", () => {
+        expect(leapyear(2001)).toEqual(false);
     });
 });
 
--- leapyear/src/leapyear.ts
+++ leapyear/src/leapyear.ts
@@ -1,6 +1,6 @@
 export function leapyear(number: number): boolean {
-    if (number % 4 === 0) return true;
-    if (number === 9) {
-        return false;
+    if (number % 4 === 0){
+        return true;
     }
+    return false;
 }
