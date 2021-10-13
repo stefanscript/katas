11c0f930ec503963fff0ee322aed5d6dba176be9
stefanscript <sha32768@gmail.com>
2021-10-13 22:52:29 +0100

    [leapyear] refactor

--- leapyear/src/leapyear.ts
+++ leapyear/src/leapyear.ts
@@ -1,8 +1,5 @@
 export function leapyear(number: number): boolean {
-    if (number === 4)
-        return true;
-    if (number === 8) return true;
-    if (number === 1996) return true;
+    if (number % 4 === 0) return true;
     if (number === 9) {
         return false;
     }
