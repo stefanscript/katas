3da334cfbd19729d764c12ae82cb070655cd3b98
stefanscript <sha32768@gmail.com>
2021-10-13 22:48:28 +0100

    [leapyear] drivin typical leap year

---
+++ leapyear/src/leapyear.spec.ts
@@ -0,0 +1,18 @@
+import {leapyear} from "./leapyear";
+
+describe("LeapYear", () => {
+    it("4 is a typical leap year", () => {
+        expect(leapyear(4)).toEqual(true);
+    });
+    it("8 is a typical leap year", () => {
+        expect(leapyear(8)).toEqual(true);
+    });
+    it("1996 is a typical leap year", () => {
+        expect(leapyear(1996)).toEqual(true);
+    });
+    it("9 is a normal year", () => {
+        expect(leapyear(9)).toEqual(false);
+    });
+});
+
+
