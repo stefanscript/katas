d3e3b630d63a4448e66e849c7a66ca3223210590
stefanscript <sha32768@gmail.com>
2021-10-30 14:25:41 +0100

    [roman-numerals] add tests to 1000 and acceptance test

--- agile-distilled-katas/src/roman-numerals/romanNumberals.spec.ts
+++ agile-distilled-katas/src/roman-numerals/romanNumberals.spec.ts
@@ -22,4 +22,43 @@ describe("Roman numerals", () => {
     it("returns IX for 9", () => {
         expect(toRoman(9)).toEqual("IX");
     });
+    it("returns X for 10", () => {
+        expect(toRoman(10)).toEqual("X");
+    });
+    it("returns XL for 40", () => {
+        expect(toRoman(40)).toEqual("XL");
+    });
+    it("returns L for 50", () => {
+        expect(toRoman(50)).toEqual("L");
+    });
+    it("returns XC for 90", () => {
+        expect(toRoman(90)).toEqual("XC");
+    });
+    it("returns C for 100", () => {
+        expect(toRoman(100)).toEqual("C");
+    });
+    it("returns CD for 400", () => {
+        expect(toRoman(400)).toEqual("CD");
+    });
+    it("returns D for 500", () => {
+        expect(toRoman(500)).toEqual("D");
+    });
+    it("returns CM for 900", () => {
+        expect(toRoman(900)).toEqual("CM");
+    });
+    it("returns M for 1000", () => {
+        expect(toRoman(1000)).toEqual("M");
+    });
+
+    describe("acceptance", () => {
+        it("846", () => {
+            expect(toRoman(846)).toEqual("DCCCXLVI");
+        });
+        it("1999", () => {
+            expect(toRoman(1999)).toEqual("MCMXCIX");
+        });
+        it("2008", () => {
+            expect(toRoman(2008)).toEqual("MMVIII");
+        });
+    });
 });
--- agile-distilled-katas/src/roman-numerals/romanNumerals.ts
+++ agile-distilled-katas/src/roman-numerals/romanNumerals.ts
@@ -1,4 +1,13 @@
 const numbersToRomanMap: Record<string, number> = {
+    "M": 1000,
+    "CM": 900,
+    "D": 500,
+    "CD": 400,
+    "C": 100,
+    "XC": 90,
+    "L": 50,
+    "XL": 40,
+    "X": 10,
     "IX": 9,
     "V": 5,
     "IV": 4,
