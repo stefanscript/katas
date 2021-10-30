b7d3c0d0af19b802d732d7fb9e10d43683a827ad
stefanscript <sha32768@gmail.com>
2021-10-30 13:07:25 +0100

    [roman-numerals] add test for 2

--- agile-distilled-katas/src/roman-numerals/romanNumberals.spec.ts
+++ agile-distilled-katas/src/roman-numerals/romanNumberals.spec.ts
@@ -1,9 +1,15 @@
 function toRoman(number: number) {
+    if(number === 2) {
+        return "II";
+    }
     return "I";
 }
 
 describe("Roman numerals", () => {
     it("returns I form 1", () => {
         expect(toRoman(1)).toEqual("I");
+    });
+    it("returns II form 2", () => {
+        expect(toRoman(2)).toEqual("II");
     });
 });
